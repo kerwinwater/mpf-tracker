a8059f00388ff1b55db9f15921275a6031acb6cb
kerwinwater <kerwinwater02@gmail.com>
2026-04-11 06:37:33 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // 讓 Next.js 可以讀取 data/ 目錄中的 JSON 檔案
-  output: 'export',
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
